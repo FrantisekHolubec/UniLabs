60573d52486bde402d9455e81f8f24e79ebbb37c
František Holubec <frantisek.holubec@gmail.com>
2024-05-03 20:44:44 +0200

    Utilities imported

---
+++ Utilities/Editor/InspectorPropertyExtensions.cs
@@ -0,0 +1,54 @@
+﻿// Author: František Holubec
+// Copyright (c) UniLabs
+
+using System;
+using Sirenix.OdinInspector.Editor;
+
+namespace UniLabs.Utilities.Editor
+{
+    public static class InspectorPropertyExtensions
+    {
+        public static bool HasParentObject<T>(this InspectorProperty property, Predicate<T> filter = null, bool includeSelf = false)
+        {
+            return TryGetParentObject<T>(property, out _, out _, filter, includeSelf);
+        }
+
+        public static T GetParentObject<T>(this InspectorProperty property, Predicate<T> filter = null, bool includeSelf = false)
+        {
+            TryGetParentObject(property, out var result, out _, filter, includeSelf);
+            return result;
+        }
+
+        public static bool TryGetParentObject<T>(this InspectorProperty property, out T result, Predicate<T> filter = null, bool includeSelf = false)
+        {
+            return TryGetParentObject(property, out result, out _, filter, includeSelf);
+        }
+
+        public static bool TryGetParentObject<T>(this InspectorProperty property, out T result, out InspectorProperty parentProperty, Predicate<T> filter = null, bool includeSelf = false)
+        {
+            Func<InspectorProperty, bool> predicate = filter != null ?
+                p => p?.ValueEntry?.WeakSmartValue is T tVal && filter(tVal) :
+                p => p?.ValueEntry?.WeakSmartValue is T;
+
+            parentProperty = property.FindParent(predicate, includeSelf);
+            if (parentProperty?.ValueEntry.WeakSmartValue is T tResult)
+            {
+                result = tResult;
+                return true;
+            }
+            result = default;
+            return false;
+        }
+
+        public static void ForceMarkDirty(this InspectorProperty property, bool alsoMarkRootDirty = true)
+        {
+            if (property?.ValueEntry?.WeakValues == null)
+                return;
+
+            property.ValueEntry.WeakValues.ForceMarkDirty();
+            property.ValueEntry.ApplyChanges();
+            if (alsoMarkRootDirty) property.MarkSerializationRootDirty();
+        }
+
+    }
+}
---
+++ Utilities/MathExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Author: František Holubec
+// Copyright (c) UniLabs
+
+namespace UniLabs.Utilities
+{
+    public static class MathExtensions
+    {
+        public static int PositiveModulo(this int x, int m)
+        {
+            var r = x % m;
+            return r < 0 ? r + m : r;
+        }
+
+        public static float PositiveModulo(this float x, float m)
+        {
+            var c = x % m;
+            if ((c < 0 && m > 0) || (c > 0 && m < 0)) {
+                c += m;
+            }
+            return c;
+        }
+    }
+}
